5dd50478f3c188def2523d425f97d17a6b3dbe60
myusername <jquiros90650@ufide.ac.cr>
2022-12-02 17:01:31 -0600

    Clase 11 - Commit

---
+++ app/src/main/java/com/sem08v/ui/home/UpdateLugarFragment.kt
@@ -0,0 +1,62 @@
+package com.sem08v.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.navArgs
+import com.sem08v.R
+import com.sem08v.databinding.FragmentAddLugarBinding
+import com.sem08v.databinding.FragmentUpdateLugarBinding
+import com.sem08v.viewModel.HomeViewModel
+
+class UpdateLugarFragment : Fragment() {
+
+    // Recuperar los elementos enviados
+    private val args by navArgs<UpdateLugarFragmentArgs>()
+
+    private var _binding: FragmentAddLugarBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        _binding = FragmentUpdateLugarBinding.inflate(inflater,container, false)
+
+
+        // carga de lugar
+        binding.etNombre.setText(args.lugarArg.nombre)
+        binding.etCorreo.setText(args.lugarArg.correo)
+        binding.etTelefono.setText(args.lugarArg.telefono)
+        binding.etWeb.setText(args.lugarArg.web)
+
+        binding.btUpdateLugar.setOnClickListener {updateLugar()}
+        //binding.btDeleteLugar.setOnClickListener {deleteLugar()}
+
+
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+private fun updateLugar(){
+    val nombre = binding.etNombre.text.toString()
+    val correo = binding.etCorreo.text.toString()
+    val telefono = binding.etTelefono.text.toString()
+    val web = binding.etWeb.text.toString()
+    if (nombre.isNotEmpty()){
+        val lugar = Lugar(args.lugarArg.id,nombre, correo, telefono,web)
+        HomeViewModel.guardarLugar(lugar)
+        Toast.makeText(requireContext(),getText(R.string.ms_UpdateLugar),Toast.LENGTH_LONG).show()
+
+    }
+    else{
+        Toast.makeText(requireContext(),getString(R.string.ms_FaltaValores),Toast.LENGTH_LONG).show()
+    }
+}
+
+}
